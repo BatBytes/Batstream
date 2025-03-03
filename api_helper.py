765ecbb0531c5775f270b2bc7497f9a1d576658e
BatByte <164018363+BatBytes@users.noreply.github.com>
2025-03-03 17:38:30 +0200

    api_helper.py

--- api_helper.py
+++ api_helper.py
@@ -1,8 +1,8 @@
 import requests
 def search_videos(query):
-    return requests.get("https://d679-34-106-182-231.ngrok-free.app/search?query="+query).json()
+    return requests.get("https://917d-34-106-182-231.ngrok-free.app/search?query="+query).json()
 def get_serverdownload(vid):
-    return requests.get("https://d679-34-106-182-231.ngrok-free.app/get_video?vid="+vid).json()
+    return requests.get("https://917d-34-106-182-231.ngrok-free.app/get_video?vid="+vid).json()
 
 #results = search_videos("العتاولة")
 #print(results)
